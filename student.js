b717439ae9be0a8945decac931dcec68089f855e
Afrina Tithy <afrina.tithy@gmail.com>
2026-06-08 10:21:57 +0600

    Added assignment files

---
+++ student.js
@@ -0,0 +1,9 @@
+let name = "Afrina Rahman Tithy";
+let age = 22;
+let district = "Dhaka";
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("District:", district);
+console.log("Student:", isStudent);
